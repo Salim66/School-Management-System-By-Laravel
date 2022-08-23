fd6d22110a46181bedde1968385ecc9c5fdcb31e
Salim66 <salimhasanriad8@gmail.com>
2022-08-23 15:39:19 +0600

    Fee Category Amount Part 3

--- resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
+++ resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.admin_master')
 
 @section('admin')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <div class="container-full">
@@ -41,51 +42,57 @@
                         @csrf
                         <div class="row">
                             <div class="col-12">
-                                <div class="form-group">
-                                    <h5>Fee Category <span class="text-danger">*</span></h5>
-                                    <div class="controls">
-                                        <select name="fee_category_id" id="fee_category_id" required class="form-control">
-                                            <option value="" disabled selected>Select Fee Category</option>
-                                            @foreach($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                            @endforeach
-                                        </select>
+                                <div class="add_item">
+
+                                    <div class="form-group">
+                                        <h5>Fee Category <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <select name="fee_category_id" id="fee_category_id" required class="form-control">
+                                                <option value="" disabled selected>Select Fee Category</option>
+                                                @foreach($categories as $category)
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <div class="form-group">
+                                                <h5>Student Class <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <select name="class_id" id="class_id" required class="form-control">
+                                                        <option value="" disabled selected>Select Student Class</option>
+                                                        @foreach($classes as $class)
+                                                        <option value="{{ $class->id }}">{{ $class->name }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-5">
+                                            <div class="form-group">
+                                                <h5>Fee Amount <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <input type="number" name="amount" class="form-control" required data-validation-required-message="This field is required"> </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-2" style="margin-top: 25px;">
+                                            <span class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i></span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                           <input type="submit" class="btn btn-rounded btn-primary" value="Add New">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-5">
-                                <div class="form-group">
-                                    <h5>Student Class <span class="text-danger">*</span></h5>
-                                    <div class="controls">
-                                        <select name="class_id" id="class_id" required class="form-control">
-                                            <option value="" disabled selected>Select Student Class</option>
-                                            @foreach($classes as $class)
-                                            <option value="{{ $class->id }}">{{ $class->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-5">
-                                <div class="form-group">
-                                    <h5>Fee Amount <span class="text-danger">*</span></h5>
-                                    <div class="controls">
-                                        <input type="number" name="amount" class="form-control" required data-validation-required-message="This field is required"> </div>
-                                </div>
-                            </div>
-                            <div class="col-2" style="margin-top: 25px;">
-                                <span class="btn btn-success addevenmore"><i class="fa fa-plus-circle"></i></span>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                   <input type="submit" class="btn btn-rounded btn-primary" value="Add New">
-                                </div>
-                            </div>
-                        </div>
+
                       </form>
 
                   </div>
@@ -106,4 +113,55 @@
     </div>
 </div>
 <!-- /.content-wrapper -->
+
+<!-- Add Close Contain -->
+<div style="visibility: hidden;">
+    <div class="whole_extra_item_add" id="whole_extra_item_add">
+        <div class="delete_whole_extra_item_add" id="delete_whole_extra_item_add">
+            <div class="form-row">
+                <div class="col-5">
+                    <div class="form-group">
+                        <h5>Student Class <span class="text-danger">*</span></h5>
+                        <div class="controls">
+                            <select name="class_id" id="class_id" required class="form-control">
+                                <option value="" disabled selected>Select Student Class</option>
+                                @foreach($classes as $class)
+                                <option value="{{ $class->id }}">{{ $class->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5">
+                    <div class="form-group">
+                        <h5>Fee Amount <span class="text-danger">*</span></h5>
+                        <div class="controls">
+                            <input type="number" name="amount" class="form-control" required data-validation-required-message="This field is required"> </div>
+                    </div>
+                </div>
+                <div class="col-2" style="margin-top: 25px;">
+                    <span class="btn btn-success addeventmore"><i class="fa fa-plus-circle"></i></span>
+                    <span class="btn btn-danger removeeventmore"><i class="fa fa-minus-circle"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Add Close Contain -->
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        let counter = 0;
+        $(document).on('click', '.addeventmore', function(){
+            let whole_extra_item_add = $('#whole_extra_item_add').html();
+            $('.add_item').append(whole_extra_item_add);
+            counter++;
+        });
+        $(document).on('click', '.removeeventmore', function(){
+            $('#delete_whole_extra_item_add').remove()
+            counter--;
+        });
+    });
+</script>
+
 @endsection
