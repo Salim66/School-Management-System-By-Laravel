752a251a613c29c3b9c3b8187df5fc75a130f67c
Salim66 <salimhasanriad8@gmail.com>
2022-08-23 14:51:17 +0600

    Fee Category Amount Part 2

--- resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
+++ resources/views/backend/setup/fee_amount/add_fee_amount.blade.php
@@ -8,13 +8,13 @@
       <div class="content-header">
           <div class="d-flex align-items-center">
               <div class="mr-auto">
-                  <h3 class="page-title">Fee Category</h3>
+                  <h3 class="page-title">Fee Amount</h3>
                   <div class="d-inline-block align-items-center">
                       <nav>
                           <ol class="breadcrumb">
                               <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
                               <li class="breadcrumb-item" aria-current="page">Tables</li>
-                              <li class="breadcrumb-item active" aria-current="page">Fee Category</li>
+                              <li class="breadcrumb-item active" aria-current="page">Fee Amount</li>
                           </ol>
                       </nav>
                   </div>
@@ -30,23 +30,56 @@
 
            <div class="box">
               <div class="box-header with-border">
-                <h3 class="box-title">Create Fee Category</h3>
-                <a href="{{ route('view.fee.category') }}" class="btn btn-rounded btn-success float-right">View Fee Category</a>
+                <h3 class="box-title">Create Fee Amount</h3>
+                <a href="{{ route('view.fee.amount') }}" class="btn btn-rounded btn-success float-right">View Fee Amount</a>
               </div>
               <!-- /.box-header -->
               <div class="box-body">
                 <div class="row">
                   <div class="col">
-                      <form novalidate method="POST" action="{{ route('fee.category.store') }}">
+                      <form novalidate method="POST" action="{{ route('fee.amount.store') }}">
                         @csrf
                         <div class="row">
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
-                                    <h5>Name <span class="text-danger">*</span></h5>
+                                    <h5>Fee Category <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="text" name="name" class="form-control" required data-validation-required-message="This field is required"> </div>
+                                        <select name="fee_category_id" id="fee_category_id" required class="form-control">
+                                            <option value="" disabled selected>Select Fee Category</option>
+                                            @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-5">
+                                <div class="form-group">
+                                    <h5>Student Class <span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <select name="class_id" id="class_id" required class="form-control">
+                                            <option value="" disabled selected>Select Student Class</option>
+                                            @foreach($classes as $class)
+                                            <option value="{{ $class->id }}">{{ $class->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <div class="form-group">
+                                    <h5>Fee Amount <span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <input type="number" name="amount" class="form-control" required data-validation-required-message="This field is required"> </div>
+                                </div>
+                            </div>
+                            <div class="col-2" style="margin-top: 25px;">
+                                <span class="btn btn-success addevenmore"><i class="fa fa-plus-circle"></i></span>
+                            </div>
+                        </div>
+                        <div class="row">
                             <div class="col-12">
                                 <div class="form-group">
                                    <input type="submit" class="btn btn-rounded btn-primary" value="Add New">
