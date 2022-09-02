36c3290ef059a13cebc8ffa821f09830d247695a
Salim66 <salimhasanriad8@gmail.com>
2022-09-02 18:17:30 +0600

    Student Marksheet Generate Part 5

--- resources/views/backend/report/marksheet/marksheet_pdf.blade.php
+++ resources/views/backend/report/marksheet/marksheet_pdf.blade.php
@@ -34,6 +34,7 @@
                     </div>
 
                     <div class="box-body" style="border: 1px solid #ddd; padding: 10px;">
+
                         <div class="row">
 
                             <div class="col-md-2 text-center" style="float: right">
@@ -51,6 +52,74 @@
                             <div class="col-md-12">
                                 <hr style="border: 1px solid; width: 100%; margin-bottom: 0px; color: #ddd;">
                                 <p style="text-align: right;"><strong><u><i>Print Date: </i> {{ date('d M Y') }} </u></strong></p>
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+
+                                <table border="1" style="border-color: #FFFFFF;" width="100%" cellpadding="8" cellspacing="2">
+
+                                    @php
+                                        $assign_student = \App\Models\AssignStudent::where('year_id', $all_marks[0]['year_id'])->where('class_id', $all_marks[0]['class_id'])->first();
+                                    @endphp
+
+                                    <tr>
+                                        <td width="50%">Student ID</td>
+                                        <td width="50%">{{ $all_marks[0]['id_no'] }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td width="50%">Roll No</td>
+                                        <td width="50%">{{ $assign_student->roll }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td width="50%">Name</td>
+                                        <td width="50%">{{ $all_marks[0]['student']['name'] }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td width="50%">Class</td>
+                                        <td width="50%">{{ $all_marks[0]['class']['name'] }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td width="50%">Session</td>
+                                        <td width="50%">{{ $all_marks[0]['year']['name'] }}</td>
+                                    </tr>
+
+                                </table>
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <table border="1" style="border-color: #FFFFFF;" width="100%" cellpadding="8" cellspacing="2">
+
+                                    <thead>
+                                        <tr>
+                                            <td>Letter Grade</td>
+                                            <td>Marks Interval</td>
+                                            <td>Grade Point</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        @foreach ($all_grades as $mark)
+                                            <tr>
+                                                <td>{{ $mark->grade_name }}</td>
+                                                <td>{{ $mark->start_marks }} - {{ $mark->end_marks }}</td>
+                                                <td>{{ number_format((float)$mark->grade_point,2) }} - {{ ($mark->grade_point == 5) ? (number_format((float)$mark->grade_point,2)) : (number_format((float)$mark->grade_point+1,2) - (float)0.01)}}</td>
+                                            </tr>
+                                        @endforeach
+
+                                    </tbody>
+
+                                </table>
+
                             </div>
 
                         </div>
